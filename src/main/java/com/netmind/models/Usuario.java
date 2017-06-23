35cfd3dc6f44230c5407b880bd9139aee194fb43
edwinSe <esbp1998@gmail.com>
2017-06-23 18:39:54 +0200

    com.netmind.java maquillaje-proyecto compra-tarea usuario-usuario

--- src/main/java/com/netmind/models/Usuario.java
+++ src/main/java/com/netmind/models/Usuario.java
@@ -1,29 +1,20 @@
 package com.netmind.models;
 
-import java.util.Date;
 
 public class Usuario {
 	private	int	uid;
 	private	String	nombre;
 	private	String	apellido;
 	private	String	email;
-	private	String	coquetitud;
-	private	double	saldo;
 	private	String	password;
-	private	Date	nacimiento;
-	private	int	activo;
 	
-	public Usuario(int uid, String nombre, String apellido, String email, String coquetitud, double saldo,
-			String password, Date nacimiento, int activo) {
+	public Usuario(int uid, String nombre, String apellido, String email, 
+			String password) {
 		this.uid = uid;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.email = email;
-		this.coquetitud = coquetitud;
-		this.saldo = saldo;
 		this.password = password;
-		this.nacimiento = nacimiento;
-		this.activo = activo;
 	}
 
 	//Setters getters
@@ -59,22 +50,6 @@ public class Usuario {
 		this.email = email;
 	}
 
-	public String getCoquetitud() {
-		return coquetitud;
-	}
-
-	public void setCoquetitud(String coquetitud) {
-		this.coquetitud = coquetitud;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-
 	public String getPassword() {
 		return password;
 	}
@@ -83,21 +58,6 @@ public class Usuario {
 		this.password = password;
 	}
 
-	public Date getNacimiento() {
-		return nacimiento;
-	}
-
-	public void setNacimiento(Date nacimiento) {
-		this.nacimiento = nacimiento;
-	}
-
-	public int getActivo() {
-		return activo;
-	}
-
-	public void setActivo(int activo) {
-		this.activo = activo;
-	}
 	
 
 	
